0c040f482b8996fdb733cd9a60a3977cfe33063a
Aleksandr Alekseev <alekseevavx@gmail.com>
2020-10-22 20:49:12 +0300

    rename package

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
 
-  homebridge.registerPlatform("homebridge-milight", "MiLight", MiLightPlatform);
+  homebridge.registerPlatform("homebridge-milight-relay", "MiLightRelay", MiLightPlatform);
 };
 
 function MiLightPlatform(log, config) {
